75f5ca38cd48cbb3a086eb7c9e3a2cc35de86bf9
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-20 00:21:19 +0600

    added promise chaining with Promise.any()

--- 01-promises-async-await/02-promises.js
+++ 01-promises-async-await/02-promises.js
@@ -130,3 +130,22 @@ Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
   });
 
 // Failed to fetch: TypeError: Failed to fetch
+
+// Promise Any:
+const fetchPromise1 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+);
+const fetchPromise2 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found"
+);
+const fetchPromise3 = fetch(
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+);
+
+Promise.any([fetchPromise1, fetchPromise2, fetchPromise3])
+  .then((response) => {
+    console.log(`${response.url}: ${response.status}`);
+  })
+  .catch((error) => {
+    console.error(`Failed to fetch: ${error}`);
+  });
