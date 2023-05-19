35dd64e8d6559c3737251518ca5f33e5c71c1942
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-20 02:22:15 +0600

    added callback pyramid/hell

--- 01-promises-async-await/ice cream shop/01-callbacks.js
+++ 01-promises-async-await/ice cream shop/01-callbacks.js
@@ -76,12 +76,26 @@ let order = (fruitName, callProduction) => {
 
 let production = () => {
   setTimeout(() => {
-    console.log(`Production started`);
-
+    console.log('production has started');
     setTimeout(() => {
-      console.log(`The fruit has been chopped`);
+      console.log('The fruit has been chopped');
+      setTimeout(() => {
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} Added`);
+        setTimeout(() => {
+          console.log('start the machine');
+          setTimeout(() => {
+            console.log(`Ice cream placed on ${stocks.holder[1]}`);
+            setTimeout(() => {
+              console.log(`${stocks.toppings[0]} as toppings`);
+              setTimeout(() => {
+                console.log('serve Ice cream');
+              }, 2000);
+            }, 3000);
+          }, 2000);
+        }, 1000);
+      }, 1000);
     }, 2000);
-  }, 0);
+  }, 0000);
 };
 
 // Trigger
