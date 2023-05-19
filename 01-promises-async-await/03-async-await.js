3477c044db573916c650f96faa0053b13eb04e3c
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-20 00:32:47 +0600

    initial commit

--- 01-promises-async-await/03-async-await.js
+++ 01-promises-async-await/03-async-await.js
@@ -20,3 +20,23 @@ async function fetchProducts() {
 }
 
 fetchProducts();
+
+async function fetchProducts() {
+  try {
+    const res = await fetch(
+      'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json'
+    );
+    if (!res.ok) {
+      throw new Error(`HTTP error: ${res.status}`);
+    }
+    const data = await res.json();
+    console.log(data[0].name);
+  } catch (error) {
+    console.log(`${error}`);
+  }
+}
+
+const promise = fetchProducts();
+promise.then((data) => {
+  console.log(data[0].name);
+});
